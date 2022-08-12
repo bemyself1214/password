da4915a483cebf7ce9ee92a5463a5a402e51fb8b
bemyself1214 <tina871879156@gmail.com>
2022-08-12 23:14:37 +0900

    Add files via upload

--- password.py
+++ password.py
@@ -4,7 +4,10 @@ while cnt > 0:
 	pw = input('請輸入密碼：')
 	if pw != password:
 		cnt-=1
-		print('密碼錯誤! 還有', cnt, '次機會')
+		if cnt > 0:
+			print('密碼錯誤! 還有', cnt, '次機會')
+		else:
+			print('沒機會嘗試了!要鎖帳號了啦!')
 	else:
 		print('登入成功')
 		break
